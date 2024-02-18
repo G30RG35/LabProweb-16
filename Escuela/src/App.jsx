4f04eae4abe890be2aee96025bba86c038e3e100
G30RG35 <javyj2017@hotmail.com>
2024-02-18 15:37:48 -0600

    Footer y header

--- Escuela/src/App.jsx
+++ Escuela/src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from "./Componentes/Footer/Footer";
+import { Header } from "./Componentes/Header/Header";
 import { Login } from "./Componentes/Login/Login";
 
 function App() {
   return (
     <>
+    <Header />
       <Login />
+      <Footer />
     </>
   );
 }
