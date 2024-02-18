cfebfa5a040f64f28b5dc80c5e4838fb8ebc1e5e
G30RG35 <javyj2017@hotmail.com>
2024-02-18 14:50:06 -0600

    Agreganos React Router doom

--- Escuela/src/App.jsx
+++ Escuela/src/App.jsx
@@ -1,9 +1,10 @@
 import { Crud } from "./Componentes/Crud/Crud";
-
+import { Header } from "./Componentes/Header/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Crud />
     </>
   );
