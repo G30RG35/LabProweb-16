246166318f29cba39a27d2f5ba40c953e57a8a51
G30RG35 <javyj2017@hotmail.com>
2024-02-25 09:09:52 -0600

    Eventos

--- Escuela/src/App.jsx
+++ Escuela/src/App.jsx
@@ -1,14 +1,16 @@
 import Footer from "./Componentes/Footer/Footer";
 import { Header } from "./Componentes/Header/Header";
-import { Login } from "./Componentes/Login/Login";
+import { Login } from "./Paginas/Login/Login";
 import { SelectDe } from "./Componentes/Selector de/SelectDeMaterias";
+import { Eventos } from "./Paginas/Eventos/Eventos";
 
 function App() {
   return (
     <>
     <Header />
       {/* <Login /> */}
-      <SelectDe />
+      {/* <SelectDe /> */}
+      <Eventos />
       <Footer />
     </>
   );
