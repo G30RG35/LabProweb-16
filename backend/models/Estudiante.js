db5f93f574753c74575f5544ab6b963f1e0bdd3e
AlejandroEs04 <2004.estrada.lopez@gmail.com>
2024-02-27 09:44:22 -0600

    "Commit 27/02/2024 by Alejandro Estrada"

---
+++ backend/models/Estudiante.js
@@ -0,0 +1,9 @@
+function Estudiante(nombre, apellidos, numero, correo, direccion) {
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.numero = numero;
+    this.correo = correo;
+    this.direccion = direccion;
+}
+
+export default Estudiante
