e22836357bff4c1a235a15b47cdb458d9ee527c8
AlejandroEs04 <2004.estrada.lopez@gmail.com>
2024-02-27 08:46:01 -0600

    "Commit 27/02/2024"

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+
+dotenv.config();
+
+// Configurar Cors
+const whiteList = [
+    process.env.FRONTEND_URL
+];
+
+const corsOptions = {
+    origin: function(origin, callback) {
+        if(whiteList.includes(origin)) {
+            // Esta permitido consultar la API
+            callback(null, true);
+        } else {
+            // No esta permitido a consultar la API
+            callback(new Error('Error de cors'));
+        }
+    }
+}
+
+app.use(cors(corsOptions));
+
+/** --- ROUTING --- */
+
+
+/** ZONA ADMIN */
+
+/** ZONA MAESTROS */
+
+/** ZONA ALUMNOS */
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`El servidor esta corriendo en el puerto ${PORT}`);
+})
