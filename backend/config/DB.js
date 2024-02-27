bd3e23348d45185563dfcd83cb83bbac54b40284
AlejandroEs04 <2004.estrada.lopez@gmail.com>
2024-02-27 09:44:48 -0600

    "Commit 27/02/2024"

--- backend/config/DB.js
+++ backend/config/DB.js
@@ -1,19 +1,19 @@
 import mysql from "mysql"
 
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'Alejandroe2004ms*',
-    database : 'escueladb'
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Alejandroe2004ms*',
+  database : 'escueladb'
 });
    
 connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
   
-    console.log('connected as id ' + connection.threadId);
+  console.log('connected as id ' + connection.threadId);
 });
 
 export default connection
